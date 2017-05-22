c2d748acb60c68f9198013686aae7784f3ef66b3
elisandro.cichelero <elisandro.cichelero@softfocus.com.br>
2017-05-22 15:32:51 -0300

    Melhorias na Conexao

--- ProjetoJavaWeb/src/config/Conexao.java
+++ ProjetoJavaWeb/src/config/Conexao.java
@@ -6,18 +6,20 @@ import java.sql.Statement;
 
 public class Conexao {
 	
+	private static final String url = "jdbc:postgresql://192.168.0.3/projeto_java_web";
+	private static final String usuario = "USUARIO";
+	private static final String senha = "SENHA";			
 	private static Connection con = null;
 
 	private static Connection getConnection() throws Exception {
 		if (con == null) {
 			Class.forName("org.postgresql.Driver").newInstance();
-			con = DriverManager.getConnection("jdbc:postgresql://192.168.0.3/projeto_java_web?user=USUARIO&password=SENHA");
+			con = DriverManager.getConnection(url + "?user=" + usuario + "&password=" + senha);
 		}
 		return con;
 	}
 	
 	public static Statement getStatement() throws Exception {
-		Connection con = Conexao.getConnection();
-		return con.createStatement();
+		return Conexao.getConnection().createStatement();
 	}
 }
