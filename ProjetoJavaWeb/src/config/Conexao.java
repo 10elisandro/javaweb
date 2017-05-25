dabf6cb92e91491b25fad9f172581c9892943836
elisandro.cichelero <elisandro.cichelero@softfocus.com.br>
2017-05-25 11:32:02 -0300

    Flyway

--- ProjetoJavaWeb/src/config/Conexao.java
+++ ProjetoJavaWeb/src/config/Conexao.java
@@ -4,22 +4,41 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+import org.flywaydb.core.Flyway;
+
 public class Conexao {
 	
 	private static final String url = "jdbc:postgresql://192.168.0.3/projeto_java_web";
-	private static final String usuario = "USUARIO";
-	private static final String senha = "SENHA";			
+	private static final String usuario = "";
+	private static final String senha = "";			
 	private static Connection con = null;
 
 	private static Connection getConnection() throws Exception {
 		if (con == null) {
 			Class.forName("org.postgresql.Driver").newInstance();
 			con = DriverManager.getConnection(url + "?user=" + usuario + "&password=" + senha);
+			migrarBancoDados();
 		}
 		return con;
 	}
 	
 	public static Statement getStatement() throws Exception {
 		return Conexao.getConnection().createStatement();
+	}
+	
+	private static void migrarBancoDados() {
+		try {
+			Flyway fly = new Flyway();
+			fly.setEncoding("ISO-8859-1");
+			fly.setDataSource(url, usuario, senha);
+			fly.setBaselineOnMigrate(true);
+			fly.setIgnoreFailedFutureMigration(true);
+			fly.setOutOfOrder(true);
+			fly.migrate();
+		} catch (Exception e) {
+			System.out.println("** ERRO NA MIGRACAO DE BANCO DE DADOS **");
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 }
